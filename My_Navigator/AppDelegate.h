e306f220aeb046845e7decc42e4708b8bffae352
Евгений Гостев <evgenij-gostevyandex.ru@iMac-Evgenij.local>
2018-01-28 14:04:12 +0300

    Initial Commit

---
+++ My_Navigator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My_Navigator
+//
+//  Created by Евгений Гостев on 28.01.18.
+//  Copyright © 2018 Евгений Гостев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My_Navigator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My_Navigator
+//
+//  Created by Евгений Гостев on 28.01.18.
+//  Copyright © 2018 Евгений Гостев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
